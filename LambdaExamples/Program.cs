3088e59108e1cc1ae21ecfd9bc17ac5383ecfd3e
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2011-12-14 17:52:17 +0000

    lambda example initial commit

---
+++ LambdaExamples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LambdaExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal result1 = SquareIt(2);
+            WriteIt(result1);
+
+            decimal result2 = Calculator(11m, x => x*32/12 + 1000 - 20);
+            WriteIt(result2);
+            Console.ReadLine();
+        }
+
+        private static Func<decimal, decimal> SquareIt = x => x*x;
+        private static Action<object> WriteIt = x => Console.WriteLine(x);
+
+        static decimal Calculator(decimal theNumber, Func<decimal, decimal> function )
+        {
+            return function.Invoke(theNumber);
+        }
+    }
+}
