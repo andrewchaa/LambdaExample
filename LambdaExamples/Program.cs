fb3abe8f28c9c2d18242ff6382e27f913d10e1a6
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2011-12-14 18:50:41 +0000

    action added

--- LambdaExamples/Program.cs
+++ LambdaExamples/Program.cs
@@ -14,15 +14,38 @@ namespace LambdaExamples
 
             decimal result2 = Calculator(11m, x => x*32/12 + 1000 - 20);
             WriteIt(result2);
+
+            Yell(new MonkeyBoy(), x => x.Scream());
+            Yell(new MonkeyBoy(), x => x.Cheer());
+
             Console.ReadLine();
         }
 
         private static Func<decimal, decimal> SquareIt = x => x*x;
         private static Action<object> WriteIt = x => Console.WriteLine(x);
 
-        static decimal Calculator(decimal theNumber, Func<decimal, decimal> function )
+        private static decimal Calculator(decimal theNumber, Func<decimal, decimal> function )
         {
             return function.Invoke(theNumber);
+        }
+
+        private static void Yell(MonkeyBoy item, Action<MonkeyBoy> action)
+        {
+            action.Invoke(item);
+        }
+        
+    }
+
+    public class MonkeyBoy
+    {
+        public void Scream()
+        {
+            Console.WriteLine("Developers Developers Developers!");
+        }
+
+        public void Cheer()
+        {
+            Console.WriteLine("Go Windows Seven!!!!");
         }
     }
 }
