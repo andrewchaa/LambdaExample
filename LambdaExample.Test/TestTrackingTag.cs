5dbf42b4bd71ae9a871dbdb0d948626776b09e2a
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2011-12-15 14:05:58 +0000

    add method with lambda expression

---
+++ LambdaExample.Test/TestTrackingTag.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace LambdaExample.Test
+{
+    [TestFixture]
+    public class TestTrackingTag
+    {
+        private const string Path = "http://www.totaljobs.com";
+        private const string Module = "twitter";
+
+        [Test]
+        public void ShouldAddPath()
+        {
+            var track = new WebTrackingTag();
+            track.AddPath(Path);
+
+            Assert.That(track.GetAllTags().Contains(Path), Is.True);
+        }
+
+        [Test]
+        public void ShouldAddModule()
+        {
+            var track = new WebTrackingTag();
+            track.AddPath(Path).AddModule(Module);
+
+            Assert.That(track.GetAllTags().Contains(Module), Is.True);
+        }
+
+        [Test]
+        public void ShouldAddPathWithGenericMethod()
+        {
+            var tracking = new WebTrackingTag();
+            tracking.Add(x => x.Path, Path)
+                .Add(x => x.Module, Module);
+
+
+            Assert.That(tracking.GetAllTags().Contains(Path), Is.True);
+            Assert.That(tracking.GetAllTags().Contains(Module), Is.True);
+
+        }
+
+
+
+
+    }
+}
---
+++ LambdaExample.Test/WebTrackingTag.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace LambdaExample.Test
+{
+    public class WebTrackingTag
+    {
+        private Tag _trackingTag = new Tag();
+
+        public string GetAllTags()
+        {
+            var properties = _trackingTag.GetType().GetProperties();
+            if (properties.Length == 0)
+                return string.Empty;
+
+            string tagValue = "?webtracking=true";
+
+            foreach (var property in properties)
+            {
+                string name = property.Name.ToLower();
+                string value = (string)property.GetValue(_trackingTag, null);
+                if (string.IsNullOrEmpty(value))
+                    continue;
+
+                tagValue += string.Format("&{0}={1}", name, value);
+            }
+
+            return tagValue;
+        }
+
+        public WebTrackingTag AddPath(string path)
+        {
+            _trackingTag.Path = path;
+
+            return this;
+        }
+
+        public WebTrackingTag AddModule(string module)
+        {
+            _trackingTag.Module = module;
+
+            return this;
+        }
+
+        public WebTrackingTag Add(Expression<Func<Tag, string>> tag, string value)
+        {
+            var propertyInfo = (tag.Body as MemberExpression).Member as PropertyInfo;
+            propertyInfo.SetValue(_trackingTag, value, null);
+
+            return this;
+        }
+    }
+
+    public class Tag
+    {
+        public string Path { get; set; }
+        public string Module { get; set; }
+    }
+}
